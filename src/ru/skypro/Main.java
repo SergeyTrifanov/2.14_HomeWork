e811f346b843abc5bb112d1df09ce7aa80f54bac
sergey <9258228@gmail.com>
2022-07-17 19:09:35 +0300

    без покрытия тестами

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,25 @@
 package ru.skypro;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        StringList stringList = new StringListImpl();
+        stringList.add("test 1");
+        stringList.add("test 3");
+        stringList.add("test 5");
+        stringList.add("test 7");
+        stringList.add("test 9");
+
+        stringList.remove(1);
+
+
+        System.out.println(Arrays.toString(stringList.toArray()));
+
+        stringList.clear();
+        System.out.println(stringList.size());
 
     }
 }
